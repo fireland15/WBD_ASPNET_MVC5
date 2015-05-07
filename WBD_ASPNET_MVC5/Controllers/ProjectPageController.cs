5dab08c061084f126feb2d26a37d88c7c266b964
fireland15 <fireland15@my.whitworth.edu>
2015-05-06 23:08:43 -0700

    Added Project Details Controller/page

---
+++ WBD_ASPNET_MVC5/Controllers/ProjectPageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WBD_ASPNET_MVC5.Controllers
+{
+    public class ProjectPageController : Controller
+    {
+        public ActionResult Details(string id)
+        {
+            return View();
+        }
+        //
+        // GET: /AddUserToProject/
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+	}
+}
