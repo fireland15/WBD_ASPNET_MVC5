751b42fa78da28e856e1d2d4fabc98f5454a964a
markklick206 <mklick16@my.whitworth.edu>
2015-05-07 03:00:22 -0700

    upload/download ya

--- WBD_ASPNET_MVC5/Controllers/DataFilesController.cs
+++ WBD_ASPNET_MVC5/Controllers/DataFilesController.cs
@@ -14,26 +14,50 @@ namespace WBD_ASPNET_MVC5.Controllers
 
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        public ActionResult Index() 
+        public ActionResult Upload() 
         {
-            return View();
+            var data = new DataFile();
+            data.Id = Guid.NewGuid().ToString();
+            data.UploadDate = DateTime.Now;
+            data.FileReference = "aasdasda";
+            return View(data);
         }
 
         //
         // POST: /DataFiles/
         [HttpPost]
-        public async Task<ActionResult> Index([Bind(Include = "Id,DataName,FileReference,DataCategory,Description,UploadDate")] HttpPostedFileBase upload)
+        //public async Task<ActionResult> Index([Bind(Include = "Id,DataName,FileReference,DataCategory,Description,UploadDate")]DataFile datafile, HttpPostedFileBase upload)
+        public ActionResult Upload([Bind(Include = "Id,DataName,FileReference,DataCategory,Description,UploadDate")]DataFile datafile, HttpPostedFileBase upload)
         {    
             if (ModelState.IsValid)
             {
                 if (upload != null && upload.ContentLength > 0)
                 {
-                    string path = Path.Combine("C:/Users/Forrest/Desktop/", Path.GetFileName(upload.FileName));
-                    upload.SaveAs(path);
+                    //datafile.FileReference = System.IO.Path.GetFullPath(upload.FileName);
+                    //datafile.DataName = System.IO.Path.GetFileName(upload.FileName);
+                    //datafile.DataCategory = System.IO.Path.GetExtension(upload.FileName);
+                    //string path = Path.Combine("C:/Users/i7/Desktop/", Path.GetFileName(upload.FileName));
+                    //upload.SaveAs(path);
+                    string FileName = upload.FileName;
+                    string FileContentType = upload.ContentType;
+                    byte[] filebytes = new byte[upload.ContentLength];
+                    //upload.InputStream.Read(filebytes, 0, Convert.ToInt32(upload.ContentLength));
+                    //System.IO.File.WriteAllBytes("c:/users/i7/desktop/test.jpeg", filebytes);
+                    upload.SaveAs("c:/users/i7/desktop/" + FileName);
+                    datafile.FileReference = "c:/users/i7/desktop/" + FileName;
                 }
-                return RedirectToAction("Index");
+                //db.DataFiles.Add(DataFile)
+                db.DataFiles.Add(datafile);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
             }
-            return RedirectToAction("Home", "Accounts");
+            //return RedirectToAction("Projects", "Accounts");
+            return Download(datafile);
+        }
+
+        public FileResult Download(DataFile datafile)
+        {
+            return File(datafile.FileReference, System.Net.Mime.MediaTypeNames.Application.Octet);
         }
 	}
 }
