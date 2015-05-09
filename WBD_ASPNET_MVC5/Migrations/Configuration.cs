e1c6b743b89ba150189dd03b948156413b975622
fireland15 <fireland15@my.whitworth.edu>
2015-05-09 15:09:13 -0700

    DB is now seeeded with default usernames (our names lcase) + passwords ("password")

--- WBD_ASPNET_MVC5/Migrations/Configuration.cs
+++ WBD_ASPNET_MVC5/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace WBD_ASPNET_MVC5.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WBD_ASPNET_MVC5.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WBD_ASPNET_MVC5.Models.ApplicationDbContext>
     {
@@ -26,6 +29,114 @@ namespace WBD_ASPNET_MVC5.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                roleManager.Create(new IdentityRole("Administrator"));
+            }
+
+            if (!roleManager.RoleExists("AppUser"))
+            {
+                roleManager.Create(new IdentityRole("AppUser"));
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = "Admin",
+                FirstName = "Admin",
+                LastName = "Da Administrator",
+                PhoneNumber = "1231234123",
+                SignUpDate = DateTime.Today
+            };
+
+            if (userManager.FindByName("Admin") == null)
+            {
+                var result = userManager.Create(user, "password");
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Administrator");
+                }
+            }
+
+            user = new ApplicationUser
+            {
+                UserName = "forrest",
+                FirstName = "Forrest",
+                LastName = "Ireland",
+                PhoneNumber = "5094672438",
+                SignUpDate = DateTime.Today
+            };
+
+            if (userManager.FindByName("forrest") == null)
+            {
+                var result = userManager.Create(user, "password");
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "AppUser");
+                }
+            }
+
+            user = new ApplicationUser
+            {
+                UserName = "mark",
+                FirstName = "Mark",
+                LastName = "Klick",
+                PhoneNumber = "3062314352",
+                SignUpDate = DateTime.Today
+            };
+
+            if (userManager.FindByName("mark") == null)
+            {
+                var result = userManager.Create(user, "password");
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "AppUser");
+                }
+            }
+
+            user = new ApplicationUser
+            {
+                UserName = "jared",
+                FirstName = "Jared",
+                LastName = "Miles",
+                PhoneNumber = "1234526449",
+                SignUpDate = DateTime.Today
+            };
+
+            if (userManager.FindByName("jared") == null)
+            {
+                var result = userManager.Create(user, "password");
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "AppUser");
+                }
+            }
+
+            user = new ApplicationUser
+            {
+                UserName = "garrett",
+                FirstName = "Garrett",
+                LastName = "Capachelli",
+                PhoneNumber = "9865854561",
+                SignUpDate = DateTime.Today
+            };
+
+            if (userManager.FindByName("garrett") == null)
+            {
+                var result = userManager.Create(user, "password");
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "AppUser");
+                }
+            }
         }
     }
 }
