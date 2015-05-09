dd5c31173dfb2fe115984c096845bb42a02de1ee
fireland15 <fireland15@my.whitworth.edu>
2015-05-09 15:42:43 -0700

    Added Garretts JS shit

---
+++ WBD_ASPNET_MVC5/Controllers/JavaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WBD_ASPNET_MVC5.Controllers
+{
+    public class JavaController : Controller
+    {
+        // GET: Java
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
