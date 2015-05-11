a5369997a58f86ea7f6f899faa8c03e0dd88f111
markklick206 <mklick16@my.whitworth.edu>
2015-05-11 02:08:04 -0700

    made view projects work

--- WBD_ASPNET_MVC5/Controllers/ProjectController.cs
+++ WBD_ASPNET_MVC5/Controllers/ProjectController.cs
@@ -62,6 +62,14 @@ namespace WBD_ASPNET_MVC5.Controllers
             project.Id = Guid.NewGuid().ToString();
             project.CreatedOn = DateTime.Now;
             project.OwnerID = User.Identity.GetUserId();
+            
+            var UPA = new UserProjectAssociation();
+            UPA.ProjectId = project.Id;
+            UPA.UserId = project.OwnerID;
+            UPA.DateAdded = project.CreatedOn;
+            db.UserProjectAssoc.Add(UPA);
+            db.SaveChanges();
+            
             return View(project);
         }
 
